6881630583138b13c5cf5c5dff45468bf2f1f89f
Nikhil Kumar Barik <36399086+nerdynikhil@users.noreply.github.com>
2021-01-20 01:02:38 +0530

    Day 16

---
+++ Day 16/duplicatebrackets.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner take = new Scanner(System.in);
+
+        String str = take.nextLine();
+
+        Stack < Character > st = new Stack < > ();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch != ')') {
+                st.push(ch);
+            } else {
+                if (st.peek() == '(') {
+                    System.out.println("true");
+                    return;
+                } else {
+                    while (st.peek() != '(') {
+                        st.pop();
+                    }
+                    st.pop();
+                }
+            }
+        }
+        System.out.println("false");
+    }
+
+}
